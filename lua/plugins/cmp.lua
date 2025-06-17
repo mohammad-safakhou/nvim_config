c02802e6ebaa3cc0b6428448aa351e6aa9202c7a
Safa Safakhou <safa.safakhou@mb-kw4cvhhhfh.bonial.net>
2025-06-17 16:28:52 +0200

    feat: added java lsp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,31 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip", -- for snippet support (optional but recommended)
+    "saadparwaiz1/cmp_luasnip", -- for LuaSnip completion
+    "hrsh7th/cmp-buffer", -- for buffer completions
+    "hrsh7th/cmp-path", -- for file path completions
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+  end,
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "mfussenegger/nvim-jdtls",
+    ft = "java",
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       setup = {
@@ -27,7 +31,7 @@ return {
           settings = {
             pylsp = {
               plugins = {
-                pycodestyle = { enabled = false }, -- optional, disables pep8 warnings
+                pycodestyle = { enabled = false },
                 pyflakes = { enabled = true },
                 jedi_completion = { enabled = true },
                 jedi_definition = { enabled = true },
@@ -38,12 +42,80 @@ return {
         },
         sqls = {
           on_attach = function(client)
-            -- Disable formatting to avoid conflicts with pgformatter
             client.server_capabilities.documentFormattingProvider = false
           end,
         },
         postgrestools = {},
       },
     },
+
+    config = function(_, opts)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+      local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      if ok then
+        capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+      end
+
+      require("lspconfig").util.default_config = vim.tbl_extend("force", require("lspconfig").util.default_config, {
+        capabilities = capabilities,
+      })
+
+      require("lspconfig.configs").postgrestools = require("lspconfig.server_configurations.postgrestools")
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "java",
+        callback = function()
+          local jdtls = require("jdtls")
+
+          local home = os.getenv("HOME")
+          local workspace_dir = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+
+          local fallback_java = "/Users/safa.safakhou/.asdf/installs/java/corretto-21.0.3.9.1/bin/java"
+          local java_bin = vim.fn.exepath("java")
+          local java_version_output = vim.fn.systemlist(java_bin .. " -version")
+          local major = tonumber(java_version_output[1]:match("(%d+)"))
+
+          if not major or major < 21 then
+            java_bin = fallback_java
+          end
+
+          local config = {
+            cmd = {
+              java_bin,
+              "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+              "-Dosgi.bundles.defaultStartLevel=4",
+              "-Declipse.product=org.eclipse.jdt.ls.core.product",
+              "-Dlog.protocol=true",
+              "-Dlog.level=ALL",
+              "-Xms1g",
+              "--add-modules=ALL-SYSTEM",
+              "--add-opens",
+              "java.base/java.util=ALL-UNNAMED",
+              "--add-opens",
+              "java.base/java.lang=ALL-UNNAMED",
+              "-jar",
+              vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+              "-configuration",
+              home .. "/.local/share/nvim/mason/packages/jdtls/config_mac",
+              "-data",
+              workspace_dir,
+            },
+            root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+            settings = {
+              java = {
+                signatureHelp = { enabled = true },
+                contentProvider = { preferred = "fernflower" },
+              },
+            },
+            init_options = {
+              bundles = {},
+            },
+          }
+
+          jdtls.start_or_attach(config)
+        end,
+      })
+    end,
   },
 }
